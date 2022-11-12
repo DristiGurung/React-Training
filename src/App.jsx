38761b5a4d31003415b3705c13eef1947ffa3bb6
DristiGurung <dristigurung1@gmail.com>
2022-11-12 09:19:56 +0545

    Deleted tic tac toe files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
-import Header from "./components/Header"
-import Game from './components/Game';
+
 
 function App() {
   return (
     <div className="App">
-     <Game/>
+     
     </div>
   );
 }
