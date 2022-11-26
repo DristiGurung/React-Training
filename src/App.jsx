884a44d3623f58ec5dbd285a23f0a05ef261aa39
DristiGurung <dristigurung1@gmail.com>
2022-11-26 09:37:43 +0545

    Router Router

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,42 @@ import logo from './logo.svg';
 import './App.css';
 import Header from "./components/Header"
 import Game from './components/Game';
+import { Routes,Route, Link, Outlet } from 'react-router-dom';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-     <Game/>
+    <div>
+      <nav>
+        <ul> 
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/game">Game</Link></li>
+        </ul>
+      </nav>
+     <Routes>
+     <Route path="/" element={<Home/>}> 
+      <Route path="/posts"element={<Post/>} />
+      <Route path="/game" element={<Game/>} />
+     </Route> />
+      <Route path="/game" element={<Game/>} />
+     </Routes>
     </div>
   );
 }
 
-export default App;
+function Home(){
+  return(
+    <> 
+    <div>Home page</div>
+    <div>Sidebar</div>
+    <Outlet/>
+    <div>Footer</div>
+    </>
+   
+  )
+}
+function Post(){
+  return(
+    <div> My Posts </div>
+  )
+}
+
